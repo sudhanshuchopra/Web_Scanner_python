5cc4387cb68d7cc6f43699ba2d737b8980dfe5e1
sudhanshu <sudhanshuccp@gmail.com>
2017-10-17 15:00:55 +0530

    retrieving robot.txt pf website

---
+++ roots_txt.py
@@ -0,0 +1,15 @@
+import urllib.request
+import io
+
+
+def get_robot_txt(url):
+
+	if(url.endswith('/')):
+		path=url
+	else:
+		path=url+'/'
+	req=urllib.request.urlopen(path+'robots.txt',data=None)
+	data=io.TextIOWrapper(req,encoding="utf-8")
+	return data.read()
+
+print(get_robot_txt("http://www.sudhanshuchopra.com"))
